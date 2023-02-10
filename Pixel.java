1d5bea9aedfe120d8308ac8fd5e257096c1a9c55
dav8174 <dav8174@rit.edu>
2023-02-09 20:32:23 -0500

    time to implement h

--- Pixel.java
+++ Pixel.java
@@ -1,38 +1,44 @@
 import java.util.*;
+import java.math.*;
 
 public class Pixel{
     int x;
     int y;
-    int z;
+    Double z;
     int color;
     boolean visited;
-    ArrayList<Pixel> neighbors;
+    HashMap<Pixel, Double> neighbors;
+    Pixel predecessor;
+    Double f;
+    Double g;
+    Double h;
 
-    Pixel(int x, int y, int z, int color){
+    Pixel(int x, int y, Double z, int color){
         this.x = x;
         this.y = y;
         this.z = z;
         this.color = color;
         this.visited = false;
-        this.neighbors = new ArrayList<Pixel>();
+        this.neighbors = new HashMap<Pixel, Double>();
+        this.predecessor = null;
     }
 
     public void fillNBS(Pixel[][] map){
         if(this.x > 0 && this.y > 0)
-            this.neighbors.add(map[this.x-1][this.y-1]);
+            this.neighbors.put(map[this.y-1][this.x-1], Math.sqrt(Math.pow(10.29, 2) + Math.pow(7.55, 2)));
         if(this.y > 0)
-            this.neighbors.add(map[this.x][this.y-1]);
-        if(this.x < 395 && this.y > 0)
-            this.neighbors.add(map[this.x+1][this.y-1]);
+            this.neighbors.put(map[this.y-1][this.x], 7.55);
+        if(this.x < 394 && this.y > 0)
+            this.neighbors.put(map[this.y-1][this.x+1], Math.sqrt(Math.pow(10.29, 2) + Math.pow(7.55, 2)));
         if(this.x > 0)
-            this.neighbors.add(map[this.x-1][this.y]);
-        if(this.x < 395)
-            this.neighbors.add(map[this.x+1][this.y]);
-        if(this.x > 0 && this.y < 500)
-            this.neighbors.add(map[this.x-1][this.y+1]);
-        if(this.y < 500)
-            this.neighbors.add(map[this.x][this.y+1]);
-        if(this.x < 395 && this.y < 500)
-            this.neighbors.add(map[this.x+1][this.y+1]);
+            this.neighbors.put(map[this.y][this.x-1], 10.29);
+        if(this.x < 394)
+            this.neighbors.put(map[this.y][this.x+1], 10.29);
+        if(this.x > 0 && this.y < 499)
+            this.neighbors.put(map[this.y+1][this.x-1], Math.sqrt(Math.pow(10.29, 2) + Math.pow(7.55, 2)));
+        if(this.y < 499)
+            this.neighbors.put(map[this.y+1][this.x], 7.55);
+        if(this.x < 394 && this.y < 499)
+            this.neighbors.put(map[this.y+1][this.x+1], Math.sqrt(Math.pow(10.29, 2) + Math.pow(7.55, 2)));
     }
 }
--- lab1.java
+++ lab1.java
@@ -9,8 +9,8 @@ public class lab1{
     public static void main(String[] args) throws IOException{
         BufferedImage terrain = null;
         String output_name = args[3];
-        int[][] elev = new int[500][395];
-        Queue<int[]> toVisit = new LinkedList<int[]>();
+        Double[][] elev = new Double[500][395];
+        Queue<Pixel> toVisit = new LinkedList<Pixel>();
         Pixel[][] map = new Pixel[500][395];
         int path_length = 0;
 
@@ -19,24 +19,24 @@ public class lab1{
 
             File elevation_file = new File(args[1]);
             Scanner elevSC = new Scanner(elevation_file);
-            for(int i = 0; i < 500; i++){
-                for(int j = 0; j < 400; j++){
-                    int next = new BigDecimal(elevSC.next()).intValue();
-                    if(j < 395){
-                        elev[i][j] = next;
+            for(int y = 0; y < 500; y++){
+                for(int x = 0; x < 400; x++){
+                    Double next = new BigDecimal(elevSC.next()).doubleValue();
+                    if(x < 395){
+                        elev[y][x] = next;
                     }
                 }
             }
             elevSC.close(); 
 
-            for(int i = 0; i < 500; i++){
-                for(int j = 0; j < 395; j++){
-                    map[i][j] = new Pixel(i, j, elev[i][j], terrain.getRGB(i,j));
+            for(int y = 0; y < 500; y++){
+                for(int x = 0; x < 395; x++){
+                    map[y][x] = new Pixel(x, y, elev[y][x], terrain.getRGB(x, y));
                 }
             }
-            for(int i = 0; i < 500; i++){
-                for(int j = 0; j < 395; j++){
-                    map[i][j].fillNBS(map);
+            for(int y = 0; y < 500; y++){
+                for(int x = 0; x < 395; x++){
+                    map[y][x].fillNBS(map);
                 }
             }
 
@@ -48,7 +48,7 @@ public class lab1{
                 int[] point = new int[2];
                 point[0] = Integer.parseInt(temp[0]);
                 point[1] = Integer.parseInt(temp[1]);
-                toVisit.add(point);
+                toVisit.add(map[point[1]][point[0]]);
             }
             pathSC.close();
         } catch (IOException e) {
@@ -67,8 +67,38 @@ public class lab1{
         final int footpath = -16777216; //black
         final int out_of_bounds = -3342235; //magenta
 
-        int[] startCoords = toVisit.remove();
-        while(!toVisit.isEmpty()){
+        Comparator<Pixel> pixelComparator = new Comparator<Pixel>() {
+            @Override
+            public int compare(Pixel p1, Pixel p2) {
+                return (int) (p1.f - p2.f);
+            }
+        };
+
+        while(toVisit.size() > 1){
+            PriorityQueue<Pixel> queue = new PriorityQueue<Pixel>(pixelComparator);
+            Pixel start = toVisit.remove();
+            start.f = 0.0;
+            queue.add(start);
+            Pixel target = toVisit.peek();
+            while(!queue.isEmpty()){
+                Pixel p = queue.remove();
+                p.visited = true;
+                Set<Pixel> neighbors = p.neighbors.keySet();
+                Iterator<Pixel> iterate = neighbors.iterator();
+                while(iterate.hasNext()){}
+                    Pixel current = iterate.next();
+                    current.predecessor = p;
+                    if(current.equals(target)){
+                        break;
+                    }
+                    else{
+                        current.g = current.predecessor.g + p.neighbors.get(current);
+                        //current.h =
+                    }
+                }
+
+            }
+            
             
         }
 
@@ -82,3 +112,10 @@ public class lab1{
         */
     }
 }
+
+/* 
+class func{
+    public int getG(Pixel pre, Pixel curr){
+        return pre.g + 
+    }
+}*/
